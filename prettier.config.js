de9fc5e5d9efca8ba4e9b391419754ca1742f094
Nikita Almanov <131481562+nikkeyl@users.noreply.github.com>
2024-08-10 16:22:56 +0300

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from '@archoleat/prettier-define-config';
+
+export default defineConfig({
+  overrides: [
+    {
+      files: ['*.js', '*.ts', '*.yaml'],
+      options: {
+        singleQuote: true,
+      },
+    },
+    {
+      files: ['*.json'],
+      options: {
+        trailingComma: 'none',
+      },
+    },
+  ],
+});
